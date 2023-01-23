3814cf446eef28cca71d42a01b2c57509916161e
Eunseo Choi <yses9296@gmail.com>
2023-01-24 00:09:37 +0900

    #2.4 Layout System

--- NomadWeather/App.js
+++ NomadWeather/App.js
@@ -1,15 +1,15 @@
+import React from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, Button, View, Alert } from 'react-native';
+import { StyleSheet, Text, Button, View, Alert, TextInput } from 'react-native';
 
 export default function App() {
+  const [text, onChangeText] = React.useState("");
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello React Native</Text>
-      <Button
-        title="Press me"
-        color="#f194ff"
-        onPress={() => Alert.alert('Button with adjusted color pressed')}
-      />
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: "tomato" }} ></View>
+      <View style={{ flex: 1, backgroundColor: "teal" }} ></View>
+      <View style={{ flex: 1, backgroundColor: "orange" }} ></View>
     </View>
   );
 }
@@ -23,5 +23,37 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 24,
+  },
+  loginBtn: {
+    color: "#06bcee"
+  },
+  input: {
+    width: 250,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   }
 });
+
+/*
+*
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello React Native</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        textContentType="emailAddress"
+        placeholder="Email"
+      />
+      <Button
+        title="LOGIN"
+        style={styles.loginBtn}
+        onPress={() => Alert.alert('Logging in...')}
+      />
+
+      <StatusBar style="auto" />
+    </View>
+*
+*/
---
+++ NomadWeather/README.md
@@ -0,0 +1,1 @@
+# Weather Application with expo app
